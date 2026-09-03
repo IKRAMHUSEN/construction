40c5422b5287de523dc698840c9824b33fb14cba
IKRAMHUSEN <imomin@truptman.in>
2026-09-03 15:34:49 +0530

    sanctum package integration

--- construction/frontend/src/App.jsx
+++ construction/frontend/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/frontend/Home';
 import About from './components/frontend/About';
 import Services from './components/frontend/Services';
@@ -11,6 +11,10 @@ import Projects from './components/frontend/Projects';
 import Blogs from './components/frontend/Blog';
 import './assets/css/style.scss';
 import ContactUs from './components/frontend/ContactUs';
+import Login from './components/backend/Login';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Dashboard from './components/backend/Dashboard';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,20 +23,27 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Home />} />
-             <Route path='about' element={<About />} />
-             <Route path='about' element={<About />} />
-             <Route path='services' element={<Services />} />
-             <Route path='projects' element={<Projects />} />
-             <Route path='blogs' element={<Blogs />} />
-             <Route path='contact' element={<ContactUs />} />
+          <Route path='/' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='about' element={<About />} />
+          <Route path='services' element={<Services />} />
+          <Route path='projects' element={<Projects />} />
+          <Route path='blogs' element={<Blogs />} />
+          <Route path='contact' element={<ContactUs />} />
+          <Route path='admin/login' element={<Login />} />
+          <Route path='admin/dashboard' element={<Dashboard />} />
+
+
 
 
 
         </Routes>
       </BrowserRouter>
+      <ToastContainer
+        position='top-center'
+      />
     </>
   )
-} 
+}
 
 export default App
---
+++ construction/frontend/src/components/backend/Login.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import Header from '../common/Header'
+import Footer from '../common/Footer'
+import { useForm } from "react-hook-form"
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
+
+function Login() {
+    const navigate = useNavigate();
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = async (data) => {
+        const res = await fetch("http://127.0.0.1:8000/api/authenticate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+
+        const result = await res.json()
+        if (result.status == false) {
+            toast.error(result.message)
+        }
+        else {
+            const userInfo = {
+                id: result.id,
+                token: result.token
+            }
+            localStorage.setItem('userInfo', JSON.stringify(userInfo))
+            navigate('/admin/dashboard')
+        }
+
+    }
+    return (
+        <>
+            <Header />
+            <main>
+                <div className='container my-5 d-flex justify-content-center'>
+                    <div className='login-form my-5'>
+                        <div className='card border-0 shadow'>
+                            <div className='card-body p-4'>
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <h4 className='mb-3'>Login here</h4>
+                                    <div className='mb-3'>
+                                        <label htmlFor='email' className='form-label'>Email</label>
+                                        <input
+                                            {
+                                            ...register("email", {
+                                                required: 'This field is required',
+                                                pattern: {
+                                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                                    message: "Invalid email address"
+                                                }
+                                            })
+                                            }
+                                            type="email" className={`form-control ${errors.email && 'is-invalid'}`} id='email' placeholder='Email' />
+                                        {errors.email && <p className='invalid-feedback'>{errors.email?.message}</p>}
+                                    </div>
+                                    <div className='mb-3'>
+                                        <label htmlFor='password' className='form-label'>Password</label>
+                                        <input
+                                            {
+                                            ...register("password", {
+                                                required: 'This field is required',
+                                            })
+                                            }
+
+                                            type="password" className={`form-control ${errors.email && 'is-invalid'}`} id='password' placeholder='Password' />
+                                        {errors.password && <p className='invalid-feedback'>{errors.password?.message}</p>}
+                                    </div>
+                                    <button type="submit" className='btn btn-primary'>Login</button>
+                                </form>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+export default Login
